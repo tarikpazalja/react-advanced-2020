157b0c63def286a40a8ee3f4f430522b9cd347c3
Tarik Pazalja <tarikpazalja@gmail.com>
2022-09-27 06:43:28 +0000

    Update 4 files

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+  //   useEffect(() => {
+  //     console.log("unutra");
+  //     if (value >= 5) {
+  //       document.title = `nove poruke (${value})`;
+  //     }
+  //   }, [value]);
+  //   console.log("vani");
+  useEffect(() => {
+    console.log("druga");
+  }, [value]);
+
+  return (
+    <div>
+      <h2>{value}</h2>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        dugmee
+      </button>
+    </div>
+  );
 };
 
 export default UseEffectBasics;
--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+  console.log(size);
+  useEffect();
+  return <h2>useEffectttt cleanup</h2>;
 };
 
 export default UseEffectCleanup;
